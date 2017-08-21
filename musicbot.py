800d14aef72f4f628ba5fb252deee52fe31b557d
jeffw-github <jeffreywang@Jeffreys-MacBook-Pro-2.local>
2017-08-20 17:49:41 -0700

    added multiple subreddits

--- musicbot.py
+++ musicbot.py
@@ -3,37 +3,57 @@
 
 import tweepy, sys, time, requests, praw, re
 
-filename=open('AuthFiles/tweepyConfig.txt','r')
-f = filename.readlines()
-CONSUMER_KEY = f[0].rstrip('\n')
-CONSUMER_SECRET = f[1].rstrip('\n')
-ACCESS_KEY = f[2].rstrip('\n')
-ACCESS_SECRET = f[3].rstrip('\n')
-filename.close()
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+def authenticateTwitter():
+	filename=open('tweepyConfig.txt','r')
+	f = filename.readlines()
+	filename.close()
+	CONSUMER_KEY = f[0].rstrip('\n')
+	CONSUMER_SECRET = f[1].rstrip('\n')
+	ACCESS_KEY = f[2].rstrip('\n')
+	ACCESS_SECRET = f[3].rstrip('\n')
+	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+	api = tweepy.API(auth)
+	return api
  
 
 
 def authenticateReddit():
 	print('Authenticating...')
-	reddit = praw.Reddit('./AuthFiles/musicbot')
+	reddit = praw.Reddit('musicbot')
 	return reddit
 
-def post(reddit):
+def post(reddit, api):
 	# deleteAll()
-	i = 0
+
 	for item in reddit.subreddit('trap').hot(limit = 25):
-		match = re.findall("https?:\/\/(?:www\.)?(soundcloud)?(spotify)?\.com\/.*", item.url)
+		match = re.findall("https?:\/\/(?:.*\.)?(soundcloud)?(spotify)?\.com\/.*", item.url)
 		if match:
 			print('Posting...')
-			api.update_status(item.url)
+			api.update_status("From r/trap " + item.url)
 			time.sleep(30)
-		# i += 1
-		# if i == 15:
-		# 	break
+
+	for item in reddit.subreddit('hiphopheads').hot(limit = 25):
+		match = re.findall("https?:\/\/(?:.*\.)?(soundcloud)?(spotify)?\.com\/.*", item.url)
+		if match:
+			print('Posting...')
+			api.update_status("From r/hiphopheads " + item.url)
+			time.sleep(30)
+
+	for item in reddit.subreddit('indieheads').hot(limit = 25):
+		match = re.findall("https?:\/\/(?:.*\.)?(soundcloud)?(spotify)?\.com\/.*", item.url)
+		if match:
+			print('Posting...')
+			api.update_status("From r/indieheads " + item.url)
+			time.sleep(30)
+
+	for item in reddit.subreddit('electronicmusic').hot(limit = 25):
+		match = re.findall("https?:\/\/(?:.*\.)?(soundcloud)?(spotify)?\.com\/.*", item.url)
+		if match:
+			print('Posting...')
+			api.update_status("From r/electronicmusic " + item.url)
+			time.sleep(30)
+
 
 def deleteAll():
 	for status in tweepy.Cursor(api.user_timeline).items():
@@ -44,7 +64,8 @@ def deleteAll():
 
 def main():
 	reddit = authenticateReddit()
-	post(reddit)
+	api = authenticateTwitter()
+	post(reddit, api)
 	
 
 if __name__ == '__main__':
